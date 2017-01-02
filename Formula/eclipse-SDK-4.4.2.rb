7ab66bb01d2ded6250a66b1d17ca1f338a5a238d
Andi <andi_4s@yahoo.de>
2017-01-02 20:27:37 +0100

    Update eclipse-SDK-4.4.2.rb

--- Formula/eclipse-SDK-4.4.2.rb
+++ Formula/eclipse-SDK-4.4.2.rb
@@ -1,5 +1,4 @@
 cask 'eclipse-SDK-4.4.2' do
-  //version '4.4.2,Luna:1a'
   sha256 '67eb608808e54011570d6b3c5f6ca0e360e397e80d92accc8f9e361ca7522125'
 
   url "http://archive.eclipse.org/eclipse/downloads/drops4/R-4.4.2-201502041700/download.php?dropFile=eclipse-SDK-4.4.2-macosx-cocoa-x86_64.tar.gz"
